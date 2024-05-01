8855e0ae0d620a076548c94e35227cdbeb015c96
ajay-dotnet-training <ajaykumarn0704@gmail.com>
2024-05-01 19:56:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+import "./App.css";
+import { Home, Details, Explore, PageNotFound, SearchResult } from "./pages";
+import { Header, Footer } from "./components";
+
+import { fetchDataFromApi } from "./utils/api";
+
+const App = () => {
+  const disptch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      disptch(getApiConfiguration(url));
+    });
+  };
+
+  useEffect(() => {
+    fetchApiConfig();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
