81d0ecdf2c8a29e1457cba4c49055f5df7795547
ajay-dotnet-training <ajaykumarn0704@gmail.com>
2024-05-03 10:19:45 +0530

    Updated Details Page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import "./style.scss";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+import Similar from "./carousel/Similar";
+import Recommendation from "./carousel/Recommendation";
 const Details = () => {
-  return (<div>
-    Details
-  </div>);
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoding } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+  return (
+    <div className="details">
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoding} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
+    </div>
+  );
 };
 
 export default Details;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,5 @@ export {default as Details} from './details/Details';
 export {default as Explore} from './explore/Explore';
 export {default as PageNotFound} from './404/PageNotFound';
 export {default as SearchResult} from './searchResult/SearchResult';
+export {default as TopRated} from './topRated/TopRated';
+
